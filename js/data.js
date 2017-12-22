1b854c141b1fb6fcee56b135ff5f878a06029a9d
xingkongs <ldk8259878@163.com>
2017-12-22 11:52:29 +0800

    add club and winterNocturne

---
+++ js/data.js
@@ -0,0 +1,50 @@
+
+var data = {
+    list:[
+        {"name":"张勇","company":"北汽新能源股份有限公司","position":"副总经理","address":"avatar/Zhang Yong.png"},
+        {"name":"刘金良","company":"吉利汽车集团","position":"副总裁","address":"avatar/Liu Jinliang.png"},
+        {"name":"李岩松","company":"吉利汽车集团","position":"国际业务总监","address":"avatar/Li Yansong.png"},
+        {"name":"许俊海","company":"广汽新能源","position":"副总经理","address":"avatar/Xu Junhai.png"},
+        {"name":"李广涛","company":"东风启辰汽车公司","position":"销售部部长","address":"avatar/Li Guangtao.png"},
+        {"name":"林密","company":"云度新能源","position":"常务副总经理","address":"avatar/Lin Mi.png"},
+        {"name":"夏珩","company":"小鹏汽车","position":"联合创始人兼总裁","address":"avatar/Xia Heng.png"},
+        {"name":"陆群","company":"长城华冠／前途","position":"董事长","address":"avatar/Lu Qun.png"},
+        {"name":"王宇","company":"奇点汽车","position":"创始人","address":"avatar/Wang Yu.png"},
+        {"name":"沈海寅","company":"奇点汽车","position":"创始人","address":"avatar/Shen Haiyin.png"},
+        {"name":"卫俊","company":"游侠汽车","position":"董事长","address":"avatar/Wei Jun.png"},
+        {"name":"谷峰","company":"爱驰亿维","position":"CEO","address":"avatar/Gu Feng.png"},
+        {"name":"李春","company":"电咖汽车","position":"总裁","address":"avatar/Li Chun.png"},
+        {"name":"孙卫健","company":"国机智骏汽车有限公司","position":"高级执行副总裁","address":"avatar/Sun Weijian.png"},
+        {"name":"张磊","company":"御捷汽车","position":"总裁","address":"avatar/Zhang Lei.png"},
+        {"name":"李国欣","company":"雷丁汽车","position":"董事长","address":"avatar/Li Guoxin.png"},
+        {"name":"王明旺","company":"欣旺达","position":"董事长","address":"avatar/Wang Mingwang.png"},
+        {"name":"李缜","company":"国轩高科","position":"董事长","address":"avatar/Li Zhen .png"},
+        {"name":"林新正","company":"辰韬资本","position":"董事长","address":"avatar/Lin Xinzheng.png"},
+        {"name":"李凌云","company":"博嘉投资","position":"合伙人兼董事长","address":"avatar/Li Lingyun.png"},
+        {"name":"黄永和","company":"中国汽车技术中心情报所","position":"总工程师","address":"avatar/Huang Yonghe.png"}
+    ],
+    councilList:[
+        {"name":"云度新能源",address:"guests/yunduxinnengyuan.png"},
+        {"name":"奇点汽车",address:"guests/qidianqiche.png"},
+        {"name":"多氟多集团",address:"guests/duofuduo.png"},
+        {"name":"华夏幸福",address:"guests/huaxiaxinfu.png"},
+        {"name":"吉利新能源",address:"guests/jilixinnengyuan.png"},
+        {"name":"小鹏汽车",address:"guests/xiaopengqiche.png"}
+    ],
+    normalList:[
+        {"name":"爱驰亿维",address:"guests/aichiyiwei.png"},
+        {"name":"蔚来汽车",address:"guests/weilaiqiche.png"},
+        {"name":"北汽新能源",address:"guests/beiqixinnegnyuan.png"},
+        {"name":"奇瑞新能源",address:"guests/qiruixinnengyuan.png"},
+        {"name":"江淮新能源",address:"guests/jianghuaxinnengyuan.png"},
+        {"name":"开云汽车",address:"guests/kaiyunqiche.png"},
+        {"name":"精进电动",address:"guests/jingjingdiandong.png"},
+        {"name":"大郡控制",address:"guests/dajunkongzhi.png"}
+    ],
+    yehuaList:[
+        {year:"2017","name":"春季夜话",address:"images/spring.png",link:"http://www.d1ev.com/news/qiye/51467"},
+        {year:"2017","name":"夏季夜话",address:"images/2017summer.png",link:"http://www.d1ev.com/news/qiye/58418"},
+        {year:"2017","name":"秋季夜话",address:"images/2017autumn.png",link:"javascript:;"},
+        {year:"2016","name":"工程师夜话",address:"images/2016yehua.jpg",link:"http://d1ev.com/special/forumEngineer.html"}
+    ]
+}
